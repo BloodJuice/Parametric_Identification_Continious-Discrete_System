1a35416124cf9e6e15b8c865664fb01f062079ba
BloodJuice <falcon.with.hat@gmail.com>
2023-10-07 18:17:10 +0300

    It works

--- FirstLab/gradients.py
+++ FirstLab/gradients.py
@@ -1,6 +1,6 @@
 import numpy as np
 import non_gradients
-
+from scipy.integrate import odeint
 
 class Gradients(non_gradients.IGradient):
     def __init__(self, n, N, s, tetta):
@@ -39,10 +39,99 @@ class Gradients(non_gradients.IGradient):
 
     def setTetta(self, tetta):
         self.tetta = tetta
-
+    def xTransform(self, massive):
+        m = len(massive)
+        n = len(massive[0])
+        result = []
+        for i in range(m):
+            for j in range(n):
+                result.append(massive[i][j])
+        return result
     def dxdt(self, xi, tk):
         massive = [0, 0]
         xDot = np.dot(self.F, (np.array(xi)).reshape((2, 1))) + np.dot(self.Psi, self.u[0][0])
         for i in range(len(xDot)):
             massive[i] = xDot[i][0]
         return massive
+
+    def dxdtAlpha(self, dxi, tk, *args):
+        massive = [0, 0]
+        alpha = args[0]
+        xi = [args[1], args[2]]
+        dxDot = np.dot(self.dF[alpha], xi) + np.dot(self.F, (np.array(dxi)).reshape((2, 1))) \
+                                                                                    + np.dot(self.Psi[alpha], self.u[0][0])
+        for i in range(len(dxDot)):
+            massive[i] = dxDot[i][0]
+        return massive
+
+
+    def dXi(self, tetta, params):
+        ###############___0.5____###################################
+        mode = 2
+        ytk = params["y"]
+        v = params["v"]
+        ki = params["ki"]
+        m = params['m']
+        R = params['R']
+        s = params["s"]
+        N = params["N"]
+        tk = np.arange(N)
+        tNow = []
+
+        gradX = params["gradX"]
+        self.tetta = tetta
+        F, Psi, H, o, xt0, u = gradX.getValues(mode=0)
+        dF, dPsi, dH, dR, dxt0, du_dua = gradX.getValues(mode=2)
+
+        # 2
+        gradient = (np.array([v / 2. * N * 1. / R * dR[alfa] for alfa in range(s)])).reshape(s, 1)
+
+        q = ki
+        # Point 5
+        xt = np.array(
+            [np.full(shape=2, fill_value=0, dtype=float).reshape(2, 1) for stepj in range(N)])
+        dxtk = np.array(
+            [[np.full(shape=2, fill_value=0, dtype=float).reshape(2, 1) for stepj in range(N)]for alpha in range(s)])
+
+        # dxPlusOne = np.array([0 for alpha in range(s)])
+        depsPlusOne = np.array([0 for alpha in range(s)])
+
+        for k in range(N - 1):
+            delta = np.array([0 for alpa in range(s)])  # Инициализация треугольничка
+            for i in range(0, q):
+                # Point 5
+                if k == 0:
+                    xt[i][k] = xt0
+                    for alpha in range(s):
+                        dxtk[alpha][i][k] = dxt0[alpha]
+                # Point 6
+                tNow = [tk[k], tk[k + 1]]
+                dxdtk_One = odeint(Gradients.dxdt, Gradients.xTransform(self, massive=xt[i][k]), tNow)[1]
+                xt[i][k + 1] = dxdtk_One
+
+                # Point 7
+                for alpha in range(s):
+                    dxtk[alpha][i][k] = np.dot(dF[alpha], xtk[i][k]) + np.dot(F, dxtk[alpha][i][
+                        k]) + np.dot(dPsi[alpha], u[i][k][0])
+                    depsPlusOne[alpha] = (-1) * np.dot(dH[alpha], xtk[alpha][k]) - np.dot(H,
+                                                                                               dxtk[alpha][i][k])
+
+                for j in range(int(ki)):
+                    epsPlusOne = y[i][ki][0] - np.dot(H, xPlusOne)
+                    for alpha in range(s):
+                        delta[alpha] += np.dot(np.dot(depsPlusOne[alpha].transpose(), pow(R, -1)), epsPlusOne) - \
+                                           (0.5) * np.dot(np.dot(depsPlusOne[alpha].transpose(), pow(R, -1)),
+                                                          dR[alpha]) * \
+                                           np.dot(pow(R, -1), epsPlusOne)
+            for alpha in range(s):
+                gradient[alpha] += delta[alpha]
+
+            if ki + 1 < N:
+                xtk[i][ki + 1] = xPlusOne
+                for alpha in range(s):
+                    dxtk[alpha][i][ki + 1] = dxtk[alpha][i][ki]
+            else:
+                xtk[i][ki] = xPlusOne
+                for alpha in range(s):
+                    dxtk[alpha][i][ki] = dxtk[alpha][i][ki]
+        return gradient
--- FirstLab/main.py
+++ FirstLab/main.py
@@ -39,15 +39,16 @@ def main():
     n = 2  # Размерность вектора х0
     s = 2  # Количество производных по тетта
     N = 30  # Число испытаний
-    ki = 1
+    ki = 1  # Initial number of system start
     q = 1
     m = v = 1.
-    tetta_false = [-2, 0.5]
+    tetta_false = [-2.0, 0.05]
     tetta_true = [-1.5, 1]
     tk = np.arange(N + 1)
     gradX = gr.Gradients(n, N, s=s, tetta=tetta_true)
     non_gradX = non_gr.Non_gradients(n, N, tetta_true)
     F, Psi, H, R, x0, u = gradX.getValues(mode=0)
+    gradX.getValues(mode=2)
 
     dxtdt_start = []
     x0 = [0.0, 0.0]
@@ -58,12 +59,21 @@ def main():
         else:
             dxtdt_start.append((np.array(odeint(gradX.dxdt, xTransform(dxtdt_start[i - 1]), tNow)[1])).reshape(2, 1))
 
+    dx = np.array(
+        [[np.full(shape=2, fill_value=0, dtype=float).reshape(2, 1) for stepj in range(N)]
+         for alpha in range(s)])
 
-    params = {"N": N, "ki": ki, "q": q, "y": y(dxtdt_start, {"H":H, "R":R}),"non_gradX":non_gradX, "gradX":gradX, "m":m, "v":v}
+    for i in range(len(tk) - 1):
+        for alpha in range(s):
+            tNow = [tk[i], tk[i + 1]]
+            if i == 0:
+                dx[alpha][i] = ((np.array(odeint(func=gradX.dxdtAlpha, y0=x0, t=tNow, args=(alpha, 0, 0))[1])).reshape(2, 1))
+            else:
+                c = xTransform(dxtdt_start[i - 1])
+                dx[alpha][i] = ((np.array(odeint(gradX.dxdtAlpha, xTransform(dx[alpha][i - 1]), tNow, args=(alpha, c[0], c[1]))[1])).reshape(2, 1))
+
+    params = {"N": N, "ki": ki, "q": q, "s":s, "m":m, "v":v, "y": y(dxtdt_start, {"H":H, "R":R}),"non_gradX":non_gradX, "gradX":gradX}
     print(f'Result is: \n{minimizeFirst(tetta_false, params)}')
-
-
-
     return 0
 
 main()
